f70c8862d244cec0019687a17aeea18e35d6918e
Javier Suazo <javier@acklenavenue.com>
2014-04-03 14:28:18 -0600

    Adding pic

--- src/FireTower.Presentation/App/www/App/controller/reportes.js
+++ src/FireTower.Presentation/App/www/App/controller/reportes.js
@@ -9,4 +9,18 @@ angular.module('firetower')
             .error(function(error) {
                 alert(error);
             });*/
+        $scope.takePicture = function () {
+            navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+                quality: 50,
+                destinationType: destinationType.DATA_URL
+            });
+
+            function onPhotoDataSuccess(imageData) {
+                alert(imageData);
+            }
+            
+            function onFail(message) {
+                alert('Failed because: ' + message);
+            }
+        };
     }]);
