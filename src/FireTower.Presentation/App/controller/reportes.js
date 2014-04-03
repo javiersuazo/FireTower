261ad8660ad26b4f99fc7f34c0a138164623450f
Javier Suazo <javier@acklenavenue.com>
2014-04-03 09:33:12 -0600

    Adding camera loginc

--- src/FireTower.Presentation/App/controller/reportes.js
+++ src/FireTower.Presentation/App/controller/reportes.js
@@ -1,4 +1,10 @@
 ﻿angular.module('starter.controllers',[])
     .controller('ReportesController', ['$scope', 'data', function ($scope, data) {
         $scope.reportes = data.obtener_reportes();
+        $scope.myPictures = [];
+        $scope.$watch('myPicture', function (value) {
+            if (value) {
+                myPictures.push(value);
+            }
+        }, true);
 }]);
